74dddc96a908e5953656dbddfb1187e1fedc4354
Terrance <156235769+terrance1996@users.noreply.github.com>
2024-06-24 11:14:36 +0300

    update search

--- tests/SearchConsoleApiTest.php
+++ tests/SearchConsoleApiTest.php
@@ -1,1 +1,21 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+use HannesKirsman\GoogleSearchConsole\SearchConsoleApi;
+
+$searchConsole = new SearchConsoleApi();
+$options = SearchConsoleApi::getDefaultOptions();
+$options['site_url'] = 'http://www.example.com/';
+$options['start_date'] = date('Y-m-d', strtotime("-3 days"));;
+$options['end_date'] = date('Y-m-d', strtotime("-3 days"));;
+$options['setDimensionFilterGroups'] = array(
+  'filters' => array (
+    'dimension' => 'page',
+    'operator' => 'contains',
+    'expression' => '/SUBPATH/',
+  ),
+);
+
+$rows = $searchConsole->getRows($options);
+print_r($rows);
